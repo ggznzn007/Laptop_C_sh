e1ca159404ff78c16c1c31dfda009bc05667fc8e
푸른 정 <ggznzn007@naver.com>
2020-08-09 15:05:03 +0900

    20200809

---
+++ C_Sharp_study001/001_Array.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class CSTest
+{
+    static void Main()
+    {
+        int[,] ar2 = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+        for (int i = 0; i < 3;)
+            ;
+    }
+}
---
+++ C_Sharp_study001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_study001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
